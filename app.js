4eb573cccae59889548db8c40a925e2c4e6b8614
MIchael <michael.olson756@gmail.com>
2016-09-01 13:25:27 -0600

    styling looking better

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ $scope.addComment = function(info){
         image: 'https://i.ytimg.com/vi/zMshy41lXjs/maxresdefault.jpg',
         author: 'Jackster',
         description: 'When the ski season comes around, Its always a bro tastic time.',
-        votes: 1,
+        votes: 0,
         postDate: moment().subtract(5, 'days').subtract(5, 'hours').calendar(),
         comments:[{ name:"mike",
                     comment:"This was a great article, very nice"
@@ -56,7 +56,7 @@ $scope.addComment = function(info){
         image: 'http://bloximages.newyork1.vip.townnews.com/jhnewsandguide.com/content/tncms/assets/v3/editorial/1/15/115e4234-8d89-59e3-808d-98c83a8eed8d/56faf5226f02a.image.jpg?resize=645%2C760',
         author: 'Scally McGree',
         description: 'The life of a ski bum can be extremely.......',
-        votes: 1,
+        votes: 2,
         postDate: moment().subtract(5, 'days').subtract(5, 'hours').calendar(),
         comments:[{ name:"Sara",
                     comment:"Shredddderrrs"
@@ -68,12 +68,30 @@ $scope.addComment = function(info){
         image: 'http://m5.paperblog.com/i/24/249371/for-those-about-to-ski-bum-we-salute-you-L-kGGina.jpeg',
         author: 'Jack Daniels',
         description: 'The night we ventured into our neighbors hot tub, the events that occured after are unspeakble.',
-        votes: 1,
+        votes: -2,
         postDate: moment().subtract(5, 'days').subtract(5, 'hours').calendar(),
         comments:[{ name:"Jack",
                     comment:"OMG I totes wanna be a ski bum bro"
               }]
-      }
+      },
+      {
+        title: 'Vail Aquires Whistler in 2017',
+        image: 'http://business.transworld.net/wp-content/blogs.dir/1/files/2015/08/vail-resorts-logo.jpg',
+        author: 'Samantha Smith',
+        description: 'Vail has aquired Whistler for 2017 and will include the resort of the Epic Pass',
+        votes: 4,
+        postDate: moment().subtract(5, 'days').subtract(5, 'hours').calendar(),
+        comments:[{ name:"Sam",
+                  comment:"@ChetBaker - booking tickets for next year NOW. Let's go!"
+                },
+                {name:"ChetBaker",
+                comment: "@Sam- Ya Girl! Booking right now!"
+              }
+
+            ]
+          }
+
+
     ]
 
 
